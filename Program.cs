93dee7497bc2a3b4309cdbe58fa5620443448dc2
GothicSession <70700149+GothicSession@users.noreply.github.com>
2020-09-03 15:42:24 +0300

    FirstAdd

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SortString
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string s = "Microsoft .NET Framework 2 Application Development Foundation";
+      string[] sa = s.Split(" ");
+      Array.Sort(sa);
+      s = string.Join(" ", sa); Console.WriteLine(s);
+    }
+  }
+}
